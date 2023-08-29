7c02d83350438a3d467245686e76e7836dd805b5
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-08-29 19:31:28 -0300

    set config ok - tailwind and react

--- results_summary/src/App.jsx
+++ results_summary/src/App.jsx
@@ -1,35 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="bg-green-800 text-Lightred font-custom">Your Result</p>
+      
+      76
+      of 100
+
+      Great
+      You scored higher than 65% of the people who have taken these tests.
+  
+      Summary
+
+      Reaction
+      80 / 100
+
+      Memory
+      92 / 100
+
+      Verbal
+      61 / 100
+
+      Visual
+      72 / 100
+
+      Continue
     </>
   )
 }
 
 export default App
+
+/*
+<div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+  
+  .attribution { font-size: 11px; text-align: center; }
+    .attribution a { color: hsl(228, 45%, 44%); }
+  
+*/
--- results_summary/tailwind.config.js
+++ results_summary/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+      './src/App.jsx',
+    ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Lightred: 'hsl(0, 100%, 67%)',
+        Orangeyellow: 'hsl(39, 100%, 56%)',
+        Greenteal: 'hsl(166, 100%, 37%)',
+        Cobaltblue: 'hsl(234, 85%, 45%)',
+        Lightslateblue: 'hsl(252, 100%, 67%)',
+        Lightroyalblue: 'hsl(241, 81%, 54%)',
+        Violetblue: 'hsla(256, 72%, 46%, 1)',
+        Persianblue: 'hsla(241, 72%, 46%, 0)',
+        White: 'hsl(0, 0%, 100%)',
+        Paleblue: 'hsl(221, 100%, 96%)',
+        Lightlavender: 'hsl(241, 100%, 89%)',
+        Darkgrayblue: 'hsl(224, 30%, 27%)',
+      },
+      fontFamily: {
+        custom: ['Hanken Grotesk', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
